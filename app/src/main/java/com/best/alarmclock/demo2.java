ef836c4d3bfbb32ea3d6727099a2882292d4bbd4
unknown <deya1907049@stud.kuet.ac.bd>
2023-03-27 20:53:16 +0600

    another java file added in second branch

---
+++ app/src/main/java/com/best/alarmclock/demo2.java
@@ -0,0 +1,27 @@
+package com.best.alarmclock;
+
+public class demo2 {
+    int salary;
+    private String status;
+
+    public demo2(int salary, String status) {
+        this.salary = salary;
+        this.status = status;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
